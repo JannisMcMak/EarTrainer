ee3f0304b56ac3a5838f5df3630c2aff090710fd
Jannis Lehner <lehner.jannis@web.de>
2026-05-08 11:53:28 +0200

    Navigation on root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-sdf
+<script lang="ts">
+	import { resolve } from '$app/paths';
+
+	const exercises = [{ name: 'Chords', path: '/chords' }] as const;
+</script>
+
+<div class="mt-4">
+	{#each exercises as exercise (exercise.path)}
+		<a href={resolve(exercise.path)} class="btn">{exercise.name}</a>
+	{/each}
+</div>
